ad549c99266e59a592d1f71774eef8f67bfb4414
4b1ss4l <159715585+4b1ss4l@users.noreply.github.com>
2025-06-07 23:01:56 -0300

    Delete custom-combo-maker.py

--- custom-combo-maker.py
+++
@@ -1,9 +0,0 @@
-cons = "abcdefghijklmnopqrstuvwxyz0123456789"
-
-combos = [f"{first}{second}{third}" for first in cons for second in cons for third in cons]
-
-with open("usernames.txt", "w") as file:
-    for combo in combos:
-        file.write(combo + "\n")
-
-print("Combos generated to usernames.txt")
